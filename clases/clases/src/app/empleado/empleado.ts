345a4ee1793423777bbc203e47c17de2c0fb74ab
Jennifer041995 <ms21100@ucad.edu.sv>
2025-08-20 00:30:51 -0600

    class

---
+++ clases/clases/src/app/empleado/empleado.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  imports: [],
+  templateUrl: './empleado.html',
+  styleUrl: './empleado.css'
+})
+export class Empleado {
+  nombre = "Juan";
+  apellido = "Pérez";
+  edad = 28;
+  empresa = "UCAD";
+
+  constructor(){}
+
+  getEdad(){
+    return this.edad;
+  }
+  llamar_empresa(empresa: string) {
+    
+  }
+  }
