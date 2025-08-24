55a8b510a73a656e220032c41dadd0cd046fc64d
Jennifer041995 <ms21100@ucad.edu.sv>
2025-08-23 20:33:40 -0600

    lab

--- laboratorios/jennifer_moreno/src/app/app.ts
+++ laboratorios/jennifer_moreno/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CatalogoComponent } from './catalogo/catalogo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CatalogoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('laboratorios');
+  protected readonly title = signal('jennifer_moreno');
 }
---
+++ laboratorios/jennifer_moreno/src/app/catalogo/catalogo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.html',
+  styleUrls: ['./catalogo.css']
+})
+export class CatalogoComponent {
+  companero1: string = 'Kevin Murillo';
+  companero2: string = 'Salvador Mendoza';
+  companero3: string = 'Ricardo Cardoza';
+}
