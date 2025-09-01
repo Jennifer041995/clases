6a178f3d1e773e5e1c6e7352ae21dbea81900485
Jennifer041995 <ms21100@ucad.edu.sv>
2025-08-31 23:27:44 -0600

    class

--- clases/clases/src/app/empleado/empleado.ts
+++ clases/clases/src/app/empleado/empleado.ts
@@ -9,34 +9,56 @@ import { Component } from '@angular/core';
 export class Empleado {
   nombre = "Juan";
   apellido = "Pérez";
-  edad = 28;
-  empresa = "UCAD";
+  edad = 15;
+  empresa = "NEVERIA";
   habilitar_input_property = false;
   usuario_registrado_property = false;
   texto_registro = "No hay nadie registrado";
+  dato = "";
 
   constructor(){}
 
   getEdad(){
     return this.edad;
   }
-  llamar_empresa(empresa: string) {
+
+  llamar_empresa(empresa: string){
     
   }
-  cambiar_registro() {
+
+  cambiar_registro(){
     this.usuario_registrado_property = !this.usuario_registrado_property;
-    this.set_usuario_registrado
+    //this.set_usuario_registrado();
   }
 
-  set_usuario_registrado() {
-    //this.cambiar_registro();
-    if(this.usuario_registrado_property) {
-      //alert(this.usuario_registrado_property);
-     // alert("El usuario está registrado");
-     this.texto_registro = "No hay nadie registrado";
-    }else {
-      //alert("El usuario no está registrado");
-      this.texto_registro = "Usuario en linea";
+  set_usuario_registrado(event: Event){
+    //alert(event.target);
+    //console.log(event);
+    if((<HTMLInputElement>event.target).value=="si"){
+      this.texto_registro = "El usuario se acaba de registrar";
+    }else{
+      this.texto_registro = "No hay nadie registrado";
     }
+    /*if(!this.usuario_registrado_property){
+      //alert(this.usuario_registrado_property);
+      //alert("El usuario salió de la sesión")
+      this.texto_registro = "No hay nadie registrado";
+    }else{
+      //alert("El usuario  se ha registrado");
+      //alert(this.usuario_registrado_property);
+      this.texto_registro = "Usuario en línea";
+    }*/
+  }
+
+  presionar(){
+    console.log("Boton presionado");
+  }
+
+  cambiar_desde_input(event: Event){
+    this.dato = (event.target as HTMLInputElement).value;
+  }
+
+  cambiar_empresa(event: Event){
+    this.empresa = (<HTMLInputElement>event.target).value;
   }
 }
